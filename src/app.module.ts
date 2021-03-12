246cd730c7c18778f100a012daf9bd93f22b1b0f
criss201x <cryteck_1011@hotmail.com>
2021-03-12 16:37:17 -0500

    devops: junto con @JocoolQ se agregan archivos de despliegue y se actualizan variables de entorno #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { environment } from './config/configuration';
 import { SintomasModule } from './sintomas/sintomas.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/sintomas_crud'),SintomasModule],
+  //imports: [MongooseModule.forRoot('mongodb://localhost/sintomas_crud'),SintomasModule],
+  imports: [MongooseModule.forRoot(`mongodb://${environment.USER}:${environment.PASS}@`+
+  `${environment.HOST}:${environment.PORT}/${environment.DB}?authSource=${environment.AUTH_DB}`, 
+  { useFindAndModify: false }),SintomasModule],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,10 +1,9 @@
 export const environment =  {
-/*   USER: process.env.SINTOMAS_CRUD_USER,
+  USER: process.env.SINTOMAS_CRUD_USER,
   PASS: process.env.SINTOMAS_CRUD_PASS,
   HOST: process.env.SINTOMAS_CRUD_HOST,
   PORT: process.env.SINTOMAS_CRUD_PORT,
   DB: process.env.SINTOMAS_CRUD_DB,
-  
-  AUTH_DB: process.env.SINTOMAS_CRUD_AUTH_DB */
-  HTTP_PORT: process.env.SINTOMAS_CRUD_HTTP_PORT
+  HTTP_PORT: process.env.SINTOMAS_CRUD_HTTP_PORT,
+  AUTH_DB: process.env.SINTOMAS_CRUD_AUTH_DB 
 };
